600db277728f08e51b57a55a0f86ba2a13268d45
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2024-01-08 17:04:31 +0000

    Creating a module

--- app.js
+++ app.js
@@ -1,13 +1,1 @@
-console.log(); // global
-
-setTimeout(); // calls function after delay
-
-clearTimeout(); // cancels setTimeout()
-
-setInterval(); // calls function repeatedly after delay
-
-clearInterval(); // cancels setInterval()
-
-// ^^ Global objects in JavaScript
-
-window // global object in browser e.g. window.console.log
+console.log(module);
