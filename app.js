6d080eb69d2219f4f8dd43087a776ad242f333fa
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2024-01-08 16:29:52 +0000

    Global objects

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-function sayHello(name) {
-    console.log('Hello ' + name);
-}
+console.log(); // global
 
-//sayHello('Jazib');
-console.log(window);
+setTimeout(); // calls function after delay
+
+clearTimeout(); // cancels setTimeout()
+
+setInterval(); // calls function repeatedly after delay
+
+clearInterval(); // cancels setInterval()
+
+// ^^ Global objects in JavaScript
+
+window // global object in browser e.g. window.console.log
