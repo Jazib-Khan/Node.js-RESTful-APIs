cf98b941468bf48eb30dbd3fb560d69009bfd939
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2024-01-15 16:52:35 +0000

    Events Module (updated)

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
+// Register a listener
+emitter.on('messageLogged', function() {
+    console.log('Listener called');
+});
+
 emitter.emit('messageLogged');
