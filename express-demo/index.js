15b2be647bbd78bb422c4bec566595d619ed49b4
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2024-01-18 19:28:58 +0000

    Configuration

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,3 +1,4 @@
+const config = require('config');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const Joi = require('joi');
@@ -9,6 +10,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(helmet());
+
+// Configuration
+console.log('Application Name: ' + config.get('name'));
+console.log('Mail Server: ' + config.get('mail.host'));
 
 if (app.get('env') === 'development') {
     app.use(morgan('tiny'));
