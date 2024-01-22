8d4bcd249360f671e56153b144b1f460e7a15948
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2024-01-22 18:06:36 +0000

    Replacing Callbacks with Promises

--- async-demo/index.js
+++ async-demo/index.js
@@ -14,16 +14,30 @@ function displayCommits(commits) {
     console.log(commits);
 }
 
-function getUser(id, callback) {
-    setTimeout(() => {
-        console.log('Reading a user from a database...');
-        callback({ id: id, gitHubUsername: 'Jaz' })
-    }, 2000);
+function getUser(id) {
+    return new Promise((resolve, reject) => {
+        // Kick off some async work
+        setTimeout(() => {
+            console.log('Reading a user from a database...');
+            resolve({ id: id, gitHubUsername: 'Jaz' })
+        }, 2000);
+    });
 }
 
-function getRepositories(username, callback) {
-    setTimeout(() => {
-        console.log('Calling GitHub API...');
-        callback(['repo1', 'repo2', 'repo3']);
-    }, 2000);
+function getRepositories(username) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Calling GitHub API...');
+            resolve(['repo1', 'repo2', 'repo3']);
+        }, 2000);
+    });
+}
+
+function getCommits(repo, callback) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Calling GitHub API...');
+            callback(['commit']);
+        }, 2000);
+    });
 }
