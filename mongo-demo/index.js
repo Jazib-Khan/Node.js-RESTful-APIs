7f53ef4459f8ae760312e3a31567fadb555ef6f2
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2024-01-22 20:11:05 +0000

    Models

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -11,3 +11,13 @@ const courseSchema = new mongoose.Schema({
     date: { type: Date, default: Date.now },
     isPublished: Boolean
 });
+
+const Course = mongoose.model('Course', courseSchema);
+const course = new Course({
+    name: 'Node.js',
+    author: 'Jaz',
+    tags: ['node', 'backend'],
+    isPublished: true
+});
+
+
